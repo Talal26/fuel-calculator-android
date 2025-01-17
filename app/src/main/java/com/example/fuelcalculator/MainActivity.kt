ed9079bead5e98d2b5e04a034aa81d2492c18c78
Talal Adnan <talalahmedadnan@gmail.com>
2025-01-17 18:21:49 +0000

    Layout shifts up when keyboard opens and added .gitignore

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(dimensionResource(R.dimen.outer_padding))
+                            .safeDrawingPadding()
                     )
                 }
             }
